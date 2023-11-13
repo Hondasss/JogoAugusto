52214d75cd4e88c5004b6cf4d8ecb8631eab3099
Hondasss <guihonda2004@gmail.com>
2023-11-13 11:02:23 -0300

    Projeto de mapa criado

--- mapa.py
+++ mapa.py
@@ -1,6 +1,27 @@
 class Mapa:
-    def __init__(self, teste):
-        self.teste = teste
+    #Definição da função INIT, que armazenará variáveis de controle da largura e altura
+    def __init__(self, largura, altura):
+        #Iniciando altura e largura
+        self.largura = largura 
+        self.altura = altura
 
-    def imprimirTeste (self):
-        return f"Olá, {self.teste}!"
+        #Iniciando uma matriz que representará toda a área do jogo, sendo essa ALTURA x LARGURA
+        self.plano = [['.' for coluna in range(largura)] for linha in range(altura)] #O ponto representa o valor inicial de cada célula na matriz
+        self.bordas() #O método é chamado aqui para criar as bordas desde o inicio do jogo, garantindo solidez e evitando erros
+
+    def bordas(self):
+        #Método que adiciona as bordas delimitadas por #
+        for i in range(self.largura):
+            self.plano[0][i] = '#' #Tampa
+            self.plano[self.altura - 1][i] = '#' #Fundo
+
+        for i in range(self.altura):
+            self.plano[i][0] = '#' #Lateral esquerda
+            self.plano[i][self.largura - 1] = '#' #Lateral direita 
+
+#Testando impressão do mapa - DEPOIS MUDAR PARA WHILE QUANDO PASSAR PARA O MAIN PARA FICAR IMPRIMINDO O MAPA CONTINUAMENTE
+if __name__ == "__main__":
+    game_map = Mapa(largura=25, altura=25)
+
+    for row in game_map.plano:
+        print(' '.join(row))
